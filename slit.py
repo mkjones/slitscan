ac0f4b917d88a0010131f896c096c4d54a598e05
Matt Jones <mkjones@cs.stanford.edu>
2014-03-04 02:42:23 +0000

    clean up a little

--- slit.py
+++ slit.py
@@ -4,7 +4,6 @@ import subprocess as sp
 import numpy
 import pylab
 import time
-import matplotlib.pyplot as plt
 import sys
 import re
 import Image
@@ -19,42 +18,46 @@ NUM_ROWS = 3
 # Where (of the 720 rows) should we start pulling rows from?
 ROW_INDEX = 350
 
-info_args = ('ffmpeg', '-i', FILE_NAME, '-vcodec', 'copy',
-             '-f', 'null', '/dev/null')
-pipe = sp.Popen(info_args, stdin = sp.PIPE, stdout = sp.PIPE,
-                stderr = sp.PIPE)
-(stdout, stderr) = pipe.communicate()
-matches = re.search('frame= (\d+)', stderr)
-num_frames = int(matches.group(1))
+def get_num_frames(filename):
+    info_args = ('ffmpeg', '-i', filename, '-vcodec', 'copy',
+                 '-f', 'null', '/dev/null')
+    pipe = sp.Popen(info_args, stdin = sp.PIPE, stdout = sp.PIPE,
+                    stderr = sp.PIPE)
+    (stdout, stderr) = pipe.communicate()
+    matches = re.search('frame= (\d+)', stderr)
+    return int(matches.group(1))
 
-args = [
-    'ffmpeg',
-    "-i",
-    FILE_NAME,
-    "-f",
-    "image2pipe",
-    "-pix_fmt",
-    "rgb24",
-    "-vcodec",
-    "rawvideo",
-    "-"
-]
+def get_final_array(filename, num_rows, row_index, num_frames):
 
-pipe = sp.Popen(args, stdin = sp.PIPE, stdout = sp.PIPE,
-                stderr = sp.PIPE)
-final_image = numpy.zeros((num_frames*NUM_ROWS, 1280,3), 'uint8')
-for i in xrange(num_frames):
-    image_bytes = pipe.stdout.read(1280*720*3)
-    image = numpy.fromstring(image_bytes, dtype='uint8').reshape((720, 1280, 3))
-    x1 = ROW_INDEX
-    x2 = ROW_INDEX + NUM_ROWS
+    args = [
+        'ffmpeg',
+        "-i",
+        filename,
+        "-f",
+        "image2pipe",
+        "-pix_fmt",
+        "rgb24",
+        "-vcodec",
+        "rawvideo",
+        "-"
+        ]
 
-    sub_image = image[ROW_INDEX + NUM_ROWS, 0:1280, :]
-    frame_index = NUM_ROWS*i
-    final_image[frame_index:(frame_index+NUM_ROWS)] = sub_image
+    pipe = sp.Popen(args, stdin = sp.PIPE, stdout = sp.PIPE,
+                    stderr = sp.PIPE)
+    final_image = numpy.zeros((num_frames*num_rows, 1280,3), 'uint8')
+    for i in xrange(num_frames):
+        image_bytes = pipe.stdout.read(1280*720*3)
+        image = numpy.fromstring(image_bytes, dtype='uint8').reshape((720, 1280, 3))
+        x1 = row_index
+        x2 = row_index + num_rows
 
-Image.fromarray(numpy.rot90(final_image)).save('/Users/mkjones/out.jpg')
-sys.exit()
+        sub_image = image[row_index + num_rows, 0:1280, :]
+        frame_index = num_rows*i
+        final_image[frame_index:(frame_index+NUM_ROWS)] = sub_image
 
-plt.imshow(numpy.rot90(final_image))
-plt.savefig('/Users/mkjones/figure.jpg')
+    return numpy.rot90(final_image)
+
+if __name__ == '__main__':
+    num_frames = get_num_frames(FILE_NAME)
+    final_image = get_final_array(FILE_NAME, NUM_ROWS, ROW_INDEX, num_frames)
+    Image.fromarray(final_image).save('/Users/mkjones/slitscan/out.jpg')
