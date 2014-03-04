8382c7e091f5779ca46009de30d8466f91f9770b
Matt Jones <mkjones@cs.stanford.edu>
2014-03-04 19:20:30 +0000

    output one image per slit position in original video. also swap python version

--- slit.py
+++ slit.py
@@ -1,4 +1,4 @@
-#!/usr/local/Cellar/python/2.7.3/bin/python
+#!/usr/local/Cellar/python/2.7.6/bin/python
 
 import subprocess as sp
 import numpy
@@ -17,7 +17,7 @@ ROW_INDEX = 200
 
 # True if your video has the "top" to the left
 # False if it has the "top" to the right.
-LEFT_IS_TOP = True
+TOP_IS_LEFT = True
 
 def get_num_frames(filename):
     info_args = ('ffmpeg', '-i', filename, '-vcodec', 'copy',
@@ -28,8 +28,7 @@ def get_num_frames(filename):
     matches = re.search('frame= *(\d+)', stderr)
     return int(matches.group(1))
 
-def get_final_array(filename, num_rows, row_index, num_frames, top_is_left):
-
+def yield_frames(filename, num_frames, top_is_left):
     args = [
         'ffmpeg',
         "-i",
@@ -45,26 +44,37 @@ def get_final_array(filename, num_rows, row_index, num_frames, top_is_left):
 
     pipe = sp.Popen(args, stdin = sp.PIPE, stdout = sp.PIPE,
                     stderr = sp.PIPE, bufsize=1280*720*3)
-    final_image = numpy.zeros((num_frames*num_rows, 1280,3), 'uint8')
+
     for i in xrange(num_frames):
         image_bytes = pipe.stdout.read(1280*720*3)
         image = numpy.fromstring(image_bytes, dtype='uint8').reshape((720, 1280, 3))
         if top_is_left:
             image = numpy.rot90(image)
             image = numpy.rot90(image)
+        yield image
+
+def get_final_array(frames, num_rows, row_index, num_frames):
+    i = 0
+    final_image = numpy.zeros((num_frames*num_rows, 1280,3), 'uint8')
+    for image in frames:
         x1 = row_index
         x2 = row_index + num_rows
 
         sub_image = image[row_index + num_rows, 0:1280, :]
         frame_index = num_rows*i
         final_image[frame_index:(frame_index+NUM_ROWS)] = sub_image
+        i += 1
 
     return numpy.rot90(final_image)
 
 if __name__ == '__main__':
     num_frames = get_num_frames(FILE_NAME)
-    final_image = get_final_array(FILE_NAME, NUM_ROWS, ROW_INDEX, num_frames, TOP_IS_LEFT)
-    name = FILE_NAME.split('/')[-1]
-    name = name.split('.')[0]
-    name = '/Users/mkjones/slitscan/%s-%d-%d.png' % (name, NUM_ROWS, ROW_INDEX)
-    Image.fromarray(final_image).save(name)
+    frames = yield_frames(FILE_NAME, num_frames, TOP_IS_LEFT)
+    frames = [x for x in frames]
+    for row_index in xrange(720 - NUM_ROWS):
+        print "processing row index %d" % row_index
+        final_image = get_final_array(frames, NUM_ROWS, row_index, num_frames)
+        name = FILE_NAME.split('/')[-1]
+        name = name.split('.')[0]
+        name = '/Users/mkjones/slitscan/%s-%d-%03d.png' % (name, NUM_ROWS, row_index)
+        Image.fromarray(final_image).save(name)
