b8bcd10fd85442dd563fe07ea0beee99aa48b637
Matt Jones <mkjones@cs.stanford.edu>
2014-03-13 18:00:10 +0000

    add some comments, try it on a different video

--- slit.py
+++ slit.py
@@ -6,14 +6,12 @@ import re
 import Image
 
 # the file from which we read. Expected to be a video of
-# width 1280px x 720px.
+# size 1280px x 720px.
+FILE_NAME = '/Users/mkjones/Movies/spinning_in_chair.mov'
 FILE_NAME = '/Users/mkjones/Movies/outside_work_for_slit.MP4'
 
 # How many rows should we take from each frame?
 NUM_ROWS = 1
-
-# Where (of the 720 rows) should we start pulling rows from?
-ROW_INDEX = 200
 
 # True if your video has the "top" to the left
 # False if it has the "top" to the right.
@@ -65,12 +63,17 @@ def get_final_array(frames, num_rows, row_index, num_frames):
         final_image[frame_index:(frame_index+NUM_ROWS)] = sub_image
         i += 1
 
+    return final_image
     return numpy.rot90(final_image)
 
 if __name__ == '__main__':
     num_frames = get_num_frames(FILE_NAME)
     frames = yield_frames(FILE_NAME, num_frames, TOP_IS_LEFT)
+
+    # only fetch the frames from ffmpeg once, and store them in memory
     frames = [x for x in frames]
+
+    # for every slit in the original video, generate a slitscan image
     for row_index in xrange(720 - NUM_ROWS):
         print "processing row index %d" % row_index
         final_image = get_final_array(frames, NUM_ROWS, row_index, num_frames)
