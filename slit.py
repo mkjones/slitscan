77665424e5d133855ad716a5fed40e7de4c0771f
Matt Jones <mkjones@cs.stanford.edu>
2014-03-16 02:05:12 +0000

    add ability to specify slit position on command line

--- slit.py
+++ slit.py
@@ -6,11 +6,7 @@ import re
 import Image
 from Video import Video
 import argparse
-
-# How many consecutive rows should we take from each frame?
-# (Useful if a video does not have many frames and the output image
-# ends up looking too horizontally squished)
-NUM_ROWS = 2
+import sys
 
 # True if your video has the "top" to the left
 # False if it has the "top" to the right.
@@ -38,20 +34,31 @@ def get_final_array(frames, num_rows, row_index, num_frames):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert video to slitscan image.')
-    parser.add_argument('-f', metavar='filename', required=True,
+    parser.add_argument('-f', metavar='filename', required=True, type=argparse.FileType(),
                         help='The filename to the video')
     parser.add_argument('-r', metavar='num_pixel_rows_per_frame', type=int, default=1,
                         help='How many consecutive rows of pixels should we take from '
                         'each frame? Increase if your image is too narrow. Anything '
                         'greater than 1 will make things look a bit jagged.')
+    parser.add_argument('-p', metavar='slit_position', type=float, default=0.5,
+                        help="Where in the frame is the slit?  Should be a decimal from "
+                        "[0, 1] representing the percent offset from the left of the frame.")
+
     args = parser.parse_args()
-    filename = args.f
+    file = args.f
+    filename = file.name
     num_rows = args.r
+    slit_position = args.p
+
+    if (slit_position < 0 or slit_position > 1):
+        print >>sys.stderr, "Invalid slit position %0.2f.  Must be on [0, 1]." % slit_position
+        parser.print_usage(sys.stderr)
+        sys.exit(1)
 
     video = Video(filename)
     num_frames = video.getNumFrames()
     frames = video.yieldFrames()
-    final_image = get_final_array(frames, num_rows, 400, num_frames)
+    final_image = get_final_array(frames, num_rows, slit_position*video.height, num_frames)
 
     filename_parts = filename.split('/')
     name = filename_parts[-1]
@@ -59,18 +66,17 @@ if __name__ == '__main__':
     image_path = '%s/%s.png' % ('/'.join(filename_parts[0:-1]), name)
 
     Image.fromarray(final_image).save(image_path)
-    print 'saved in %s' % image_path
-    import sys
+    print image_path
     sys.exit()
 
     # only fetch the frames from ffmpeg once, and store them in memory
     frames = [x for x in frames]
 
     # for every slit in the original video, generate a slitscan image
-    for row_index in xrange(720 - num_rows):
+    for row_index in xrange(0, 720 - num_rows, 4):
         print "processing row index %d" % row_index
-        final_image = get_final_array(frames, NUM_ROWS, row_index, num_frames)
+        final_image = get_final_array(frames, num_rows, row_index, num_frames)
         name = filename.split('/')[-1]
         name = name.split('.')[0]
-        name = '/Users/mkjones/slitscan/%s-%d-%03d.png' % (name, NUM_ROWS, row_index)
+        name = '/Users/mkjones/slitscan/%s-%d-%03d.png' % (name, num_rows, row_index)
         Image.fromarray(final_image).save(name)
